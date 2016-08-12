5d9718c3849704fa71d955f778533d428d38d7dd
zdf1018475062 <zhangdefu@zhangdefudeMacBook-Pro.local>
2016-08-12 16:00:37 +0800

    新建项目

---
+++ DFMiGu/DFMiGu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DFMiGu
+//
+//  Created by 张德孚 on 16/8/12.
+//  Copyright © 2016年 Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DFMiGu/DFMiGu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DFMiGu
+//
+//  Created by 张德孚 on 16/8/12.
+//  Copyright © 2016年 Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
